128ed4bb355e979cf3dd722c1a61adb0c8b4bfef
Syed Umar <khan.afrin02@gmail.com>
2023-08-31 12:14:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Maha } from './Component/Maha';
+
+function App() {
+  return (
+    
+  <div>
+    <h3>Assigment 04</h3>
+    <Maha/>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Maha.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+ export const Maha=()=>{
+    return(
+        <div className="child">
+            <h4>Maharashtara</h4>
+            <ul>
+                <li>Mumbai</li>
+                <li>Pune</li>
+                <li>Nagpur</li>
+                <li>Aurangabad</li>
+                <li>Nanded</li>
+            </ul>
+
+        </div>
+    )
+}
